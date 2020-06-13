d871d395e5a232875ca4f2421f11eede3ecf2e37
joel <oupenguin@gmail.com>
2020-06-13 17:13:13 +0800

    refactor: router配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Dashboard from '../views/dm-dashboard'
 
 Vue.use(VueRouter)
 
@@ -8,24 +7,13 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
-    redirect: 'dashboard',
-    children: [
-      {
-        path: '/dashboard',
-        component: () => import('@/views/dm-dashboard')
-      }
-    ]
+    component: () => import('@/layout'),
+    redirect: '/dashboard',
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: function () {
-  //     return import(/* webpackChunkName: "about" */ '../views/About.vue')
-  //   }
-  // }
+  {
+    path: '/dashboard',
+    component: () => import('@/views/dm-dashboard')
+  }
 ]
 
 const router = new VueRouter({
