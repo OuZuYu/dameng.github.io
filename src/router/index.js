c90ca573029cacad1351928c7324219a9c721c36
joel <oupenguin@gmail.com>
2020-06-13 16:21:15 +0800

    refactor: 路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Dashboard from '../views/dm-dashboard'
 
 Vue.use(VueRouter)
 
@@ -8,7 +8,13 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
-    component: Home
+    redirect: 'dashboard',
+    children: [
+      {
+        path: '/dashboard',
+        component: () => import('@/views/dm-dashboard')
+      }
+    ]
   },
   // {
   //   path: '/about',
