337629f4897847e4503b5ccdf3d4e882538a0fb0
joel <oupenguin@gmail.com>
2020-06-13 18:51:17 +0800

    feat: current

--- src/container/dashboard/dm-current.vue
+++ src/container/dashboard/dm-current.vue
@@ -1,12 +1,38 @@
 <template>
   <div>
-
+    <block-header block-title="current"></block-header>
+    <line-item :data="currentData"></line-item>
   </div>
 </template>
 
 <script>
-export default {
+import BlockHeader from '@/components/block-header'
+import LineItem from '@/components/line-item'
 
+export default {
+  components: {
+    BlockHeader,
+    LineItem
+  },
+
+  data() {
+    return {
+      currentData: [
+        {
+          name: '放大范德萨范德萨第三方打撒撒旦发',
+          money: 123213
+        },
+        {
+          name: '123132dsa',
+          money: 2333333
+        },
+        {
+          name: '123132dsa',
+          money: 333333
+        }
+      ]
+    }
+  }
 }
 </script>
 
