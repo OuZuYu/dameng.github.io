2382602cb25e380a5ca4e48d39f433407138b222
joel <oupenguin@gmail.com>
2020-06-13 18:50:07 +0800

    chore: 引入插件

--- src/main.js
+++ src/main.js
@@ -5,11 +5,17 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+import '@/styles/index.less' // global css
+
+
 // 应该按需引入，时间原因，先完整引入
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
 Vue.use(ElementUI);
+
+// 应该按需引入，时间原因，先完整引入
+import echarts from "echarts";
+Vue.prototype.$echarts = echarts;
 
 new Vue({
   router,
