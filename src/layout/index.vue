69eefd9cc4544e3eb7626226a3a722189bb0ff24
joel <oupenguin@gmail.com>
2020-06-13 16:13:42 +0800

    refactor: 整体布局

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,22 +1,39 @@
 <template>
   <div class="dm-app">
-    <app-header></app-header>
-    <app-main></app-main>
+    <div class="app-left">
+      <app-header></app-header>
+      <app-main></app-main>
+    </div>
+
+    <div class="app-right">
+      <right-panel></right-panel>
+    </div>
   </div>
 </template>
 
 <script>
 import AppHeader from './app-header'
 import AppMain from './app-main'
+import RightPanel from './right-panel'
 
 export default {
   components: {
     AppHeader,
-    AppMain
+    AppMain,
+    RightPanel
   }
 }
 </script>
 
 <style scoped lang="less">
+.dm-app {
+  display: flex;
+  .app-left {
+    flex: 1;
+  }
 
+  .app-right {
+    flex: 0 0 500px;
+  }
+}
 </style>
