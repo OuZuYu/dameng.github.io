11cb694702b07dd9b9ef3ae436d7deeb865b9b09
joel <oupenguin@gmail.com>
2020-06-13 18:50:24 +0800

    feat: block-header组件

---
+++ src/components/block-header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="block-header">
+    <h3 class="title">{{blockTitle}}</h3>
+    <span class="title">{{viewAll}}</span>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    blockTitle: String,
+    viewAllText: String
+  },
+
+  computed: {
+    viewAll() {
+      return this.viewAllText || 'View all'
+    }
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.block-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
