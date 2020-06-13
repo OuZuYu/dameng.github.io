b27655354aa263bf5ce909c15ab11fad9f0a1512
joel <oupenguin@gmail.com>
2020-06-13 16:14:02 +0800

    feat: header

--- src/layout/app-header.vue
+++ src/layout/app-header.vue
@@ -1,15 +1,44 @@
 <template>
-  <div>
+  <div class="app-header">
+    <img class="logo" src="http://placehold.it/60x60" alt="logo">
 
+    <navbar class="navbar"></navbar>
+
+    <!-- <div class="search"></div> -->
+    <el-input
+      class="header-input"
+      placeholder="search..."
+      v-model="input"
+      clearable>
+      <el-button slot="append" icon="el-icon-search"></el-button>
+    </el-input>
   </div>
 </template>
 
 <script>
-export default {
+import Navbar from './nav-bar'
 
+export default {
+  components: {
+    Navbar
+  }
 }
 </script>
 
 <style scoped lang="less">
+.app-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
+  .logo {
+
+  }
+  .navbar {
+    // flex: 1;
+  }
+  .header-input {
+    flex: 0 0 400px;
+  }
+}
 </style>
