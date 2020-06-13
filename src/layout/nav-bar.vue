694abdd41b1949d5b228cffbc63c3ba2d403dda7
joel <oupenguin@gmail.com>
2020-06-13 16:13:54 +0800

    feat: 导航栏

--- src/layout/nav-bar.vue
+++ src/layout/nav-bar.vue
@@ -1,12 +1,44 @@
 <template>
   <div>
+  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+    <el-menu-item v-for="(nav, index) in navs" :index="nav.name" :key="index">
+      {{nav.name}}
+    </el-menu-item>
 
+  </el-menu>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      navs: [
+        {
+          name: 'Dashboard',
+          children:[]
+        },
+        {
+          name: 'Services',
+          children:[]
+        },
+        {
+          name: 'History',
+          children:[]
+        },
+        {
+          name: 'Actions',
+          children:[]
+        },
+      ],
+      activeIndex: 'Dashboard'
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
 }
 </script>
 
