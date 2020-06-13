2b0821f35c79df48e7191f7ddad11cfc24fd27d4
joel <oupenguin@gmail.com>
2020-06-13 15:54:01 +0800

    init

---
+++ src/layout/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dm-app">
+    <app-header></app-header>
+    <app-main></app-main>
+  </div>
+</template>
+
+<script>
+import AppHeader from './app-header'
+import AppMain from './app-main'
+
+export default {
+  components: {
+    AppHeader,
+    AppMain
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+// 应该按需引入，时间原因，先完整引入
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI);
+
 new Vue({
   router,
   store,
